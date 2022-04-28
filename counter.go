79a6e429d5cd8e45a67246824db6b1407633571e
Roman Kunin <kunin.ra@dns-shop.ru>
2022-04-28 17:10:55 +1000

    init

---
+++ counter.go
@@ -0,0 +1,29 @@
+package counter
+
+import (
+	"sync"
+)
+
+var mu sync.Mutex
+
+var Counter int
+
+func Inc() {
+	mu.Lock()
+	Counter++
+	mu.Unlock()
+}
+
+func Dec() {
+	mu.Lock()
+	Counter--
+	mu.Unlock()
+}
+
+var Values []int
+
+func AddValue(v int) {
+	mu.Lock()
+	Values = append(Values, v)
+	mu.Unlock()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mosceo/counter
+
+go 1.18
